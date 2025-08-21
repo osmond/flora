10fdea9142d96111f74f6e914d5700ddfb84e8a3
Jon Osmond <jonathan.osmond@gmail.com>
2025-08-20 23:45:09 -0500

    chore: add tailwindcss animate plugin

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "tailwindcss";
+import animate from "tailwindcss-animate";
+
+export default {
+  darkMode: ["class"],
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [animate],
+} satisfies Config;
