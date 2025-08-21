70bc7af8db76442897761759e533b6a7260f408e
Jon Osmond <jonathan.osmond@gmail.com>
2025-08-21 12:59:11 -0500

    fix: avoid config naming conflict

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
+import config from "./config";
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  config.NEXT_PUBLIC_SUPABASE_URL,
+  config.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 );
--- src/lib/supabaseAdmin.ts
+++ src/lib/supabaseAdmin.ts
@@ -1,7 +1,8 @@
 import { createClient, type SupabaseClient } from "@supabase/supabase-js";
+import config from "./config";
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const serviceRole = process.env.SUPABASE_SERVICE_ROLE_KEY!; // server-only
+const url = config.NEXT_PUBLIC_SUPABASE_URL;
+const serviceRole = config.SUPABASE_SERVICE_ROLE_KEY; // server-only
 
 declare global {
   var supabaseAdmin: SupabaseClient | undefined;
