cb338904bdc3666ff3cadc0fb793e8641e566fcb
Jon Osmond <jonathan.osmond@gmail.com>
2025-08-22 00:00:47 -0500

    feat: remove analytics from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
 import TodayPage, { revalidate } from "./today/page";
-import AnalyticsPanel from "@/components/analytics/AnalyticsPanel";
 
 export { revalidate };
 
 export default function Page() {
-  return (
-    <div className="space-y-8">
-      <AnalyticsPanel />
-      <TodayPage />
-    </div>
-  );
+  return <TodayPage />;
 }
 
