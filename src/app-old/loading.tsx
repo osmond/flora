dead240b3d6c403a3dd789836bac0ca108140893
osmond <j@osmond.cc>
2025-08-24 09:38:48 -0500

    delete

--- src/app-old/loading.tsx
+++
@@ -1,8 +0,0 @@
-// src/app/loading.tsx
-export default function Loading() {
-  return (
-    <div className="min-h-dvh grid place-items-center p-8">
-      <div className="text-sm text-muted-foreground">Loading…</div>
-    </div>
-  );
-}
