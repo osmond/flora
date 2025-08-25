3af664d98cba516998a3fa63275ebb458e45be33
Jon Osmond <jonathan.osmond@gmail.com>
2025-08-24 22:24:17 -0500

    feat: add cn utility

--- src/app/plants/new/page.tsx
+++ src/app/plants/new/page.tsx
@@ -1,11 +1,21 @@
-import AddPlantForm from '@/components/plant/AddPlantForm';
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
 
-export default function NewPlantPage() {
-
+export default function Page() {
   return (
-    <div className="max-w-md p-4 md:p-6 mx-auto">
-      <AddPlantForm />
+    <div className="max-w-md mx-auto space-y-4 mt-8">
+      <div className="space-y-2">
+        <Label htmlFor="name">Nickname</Label>
+        <Input id="name" placeholder="Kay" />
+      </div>
 
+      <div className="space-y-2">
+        <Label htmlFor="species">Species</Label>
+        <Input id="species" placeholder="Search species..." />
+      </div>
+
+      <Button className="w-full">Create Plant</Button>
     </div>
-  );
+  )
 }
--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,14 +1,9 @@
-'use client';
-import * as React from 'react';
-import * as TabsPrimitive from '@radix-ui/react-tabs';
-import { clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+'use client'
+import * as React from 'react'
+import * as TabsPrimitive from '@radix-ui/react-tabs'
+import { cn } from '@/lib/utils'
 
-function cn(...inputs: Array<string | undefined | null | false>) {
-  return twMerge(clsx(inputs));
-}
-
-const Tabs = TabsPrimitive.Root;
+const Tabs = TabsPrimitive.Root
 
 const TabsList = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.List>,
@@ -22,8 +17,8 @@ const TabsList = React.forwardRef<
     )}
     {...props}
   />
-));
-TabsList.displayName = TabsPrimitive.List.displayName;
+))
+TabsList.displayName = TabsPrimitive.List.displayName
 
 const TabsTrigger = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Trigger>,
@@ -37,8 +32,8 @@ const TabsTrigger = React.forwardRef<
     )}
     {...props}
   />
-));
-TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
+))
+TabsTrigger.displayName = TabsPrimitive.Trigger.displayName
 
 const TabsContent = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Content>,
@@ -52,7 +47,7 @@ const TabsContent = React.forwardRef<
     )}
     {...props}
   />
-));
-TabsContent.displayName = TabsPrimitive.Content.displayName;
+))
+TabsContent.displayName = TabsPrimitive.Content.displayName
 
-export { Tabs, TabsList, TabsTrigger, TabsContent };
+export { Tabs, TabsList, TabsTrigger, TabsContent }
