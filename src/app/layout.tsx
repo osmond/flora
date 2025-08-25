ad2c8b8fa990556cad793dc49fda995424e2e737
Jon Osmond <jonathan.osmond@gmail.com>
2025-08-24 19:00:57 -0500

    fix: ensure analytics opt-out attribute matches server

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from 'react';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" data-google-analytics-opt-out="">
       <body>{children}</body>
     </html>
   );
