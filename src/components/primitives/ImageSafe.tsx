e9b2f5d4f846e8b7c232d949c4490c8ec01ef93c
Jon Osmond <jonathan.osmond@gmail.com>
2025-08-24 23:40:03 -0500

    feat: add species autosuggest

---
+++ src/components/primitives/ImageSafe.tsx
@@ -0,0 +1,9 @@
+import Image, { ImageProps } from "next/image"
+
+/**
+ * ImageSafe: thin wrapper around next/image that allows passing width/height OR fill via className.
+ * Prefer width/height for performance. For unknown sizes, wrap parent in relative container and pass fill.
+ */
+export function ImageSafe(props: ImageProps) {
+  return <Image {...props} />
+}
