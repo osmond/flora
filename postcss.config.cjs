09f299294fb549280683a2603a7c41046297763a
Jon Osmond <jonathan.osmond@gmail.com>
2025-08-25 19:58:26 -0500

    Fix build configuration and dependencies

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-const tailwindcss = require('@tailwindcss/postcss');
-
 module.exports = {
-  plugins: [tailwindcss],
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
 };
